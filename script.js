ab4f6d13038f44d31ffc42606416f1573c45f9c3
aacho04 <aaditinarayan14@gmail.com>
2026-07-03 00:02:30 +0530

    updated

--- script.js
+++
@@ -1,5 +0,0 @@
-function toggleTheme() {
-  const root = document.documentElement;
-  const current = root.getAttribute("data-theme");
-  root.setAttribute("data-theme", current === "dark" ? "light" : "dark");
-}
